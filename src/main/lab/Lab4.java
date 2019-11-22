937a0bbc1273ae12be6ac698ef9a58d40e8b06f5
Andrew Yesipcuk <yesipchuk.andriy@gmail.com>
2019-11-22 09:08:57 +0200

    new labs

--- src/main/lab/Lab4.java
+++ src/main/lab/Lab4.java
@@ -5,12 +5,12 @@ import main.util.FileReader;
 
 public class Lab4 extends Lab{
     static {
-        INPUT = FileReader.readFile("D:\\IdeaProjects\\cryptography\\src\\res\\text3.txt");
+        INPUT = FileReader.readFile("/Users/dron/IdeaProjects/cryptography/src/res/text3.txt");
     }
 
     public static void main(String[] args) {
-        String encryptedtext = SDESEncryptor.encrypt(INPUT, 10);
-        String decryptedText = SDESEncryptor.decrypt(encryptedtext, 10);
+        String encryptedtext = SDESEncryptor.encrypt(INPUT, 51);
+        String decryptedText = SDESEncryptor.decrypt(encryptedtext, 51);
         showResult(INPUT, encryptedtext, decryptedText);
     }
 }
