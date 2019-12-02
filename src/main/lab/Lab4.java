aba82695de7160dfa9b0c6d169ce7aa931b5b16a
Roman Osadchuk <osadchuk.roma.26@gmail.com>
2019-12-02 22:18:19 +0200

    lab5(4): change millis to nanos

--- src/main/lab/Lab4.java
+++ src/main/lab/Lab4.java
@@ -5,12 +5,12 @@ import main.util.FileOperationsHelper;
 
 public class Lab4 extends Lab{
     static {
-        INPUT = FileOperationsHelper.readFile("/Users/dron/IdeaProjects/cryptography/src/res/lab9.txt");
+      //  INPUT = FileOperationsHelper.readFile("/Users/dron/IdeaProjects/cryptography/src/res/lab9.txt");
     }
 
     public static void main(String[] args) {
-        String encryptedtext = SDESEncryptor.encrypt("h", 113);
-        Lab4.decryptAttack(encryptedtext, "h");
+        String encryptedtext = SDESEncryptor.encrypt("hello", 113);
+        Lab4.decryptAttack(encryptedtext, "hello");
         String decryptedText = SDESEncryptor.decrypt(encryptedtext, 113);
         showResult(INPUT, encryptedtext, decryptedText);
     }
@@ -18,13 +18,13 @@ public class Lab4 extends Lab{
     static void decryptAttack(String encryptedText, String finalText) {
         int key = 0;
         String decryptedText = encryptedText;
-        long begin = System.currentTimeMillis();
+        long begin = System.nanoTime();
         while(!decryptedText.equals(finalText)) {
             key++;
             decryptedText = SDESEncryptor.decrypt(encryptedText, key);
             System.out.println("Key: " + key + "  DecryptedText: " + decryptedText);
         }
-        long end = System.currentTimeMillis() - begin ;
-        System.out.println(end + "ms.");
+        long end = System.nanoTime() - begin ;
+        System.out.println(end + " ns.");
     }
 }
