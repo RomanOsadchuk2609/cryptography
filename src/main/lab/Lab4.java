4d358b46972ba4628f1cf33d293d7909f8b05ec0
Andrew Yesipcuk <yesipchuk.andriy@gmail.com>
2019-12-02 15:13:26 +0200

    Lab5(probably right)

--- src/main/lab/Lab4.java
+++ src/main/lab/Lab4.java
@@ -10,7 +10,18 @@ public class Lab4 extends Lab{
 
     public static void main(String[] args) {
         String encryptedtext = SDESEncryptor.encrypt("h", 515);
-        String decryptedText = SDESEncryptor.decrypt(encryptedtext, 515);
+        Lab4.decryptAttack(encryptedtext, "h");
+        String decryptedText = SDESEncryptor.decrypt(encryptedtext, 113);
         showResult(INPUT, encryptedtext, decryptedText);
+    }
+
+    static void decryptAttack(String encryptedText, String finalText) {
+        int key = 0;
+        String decryptedText = encryptedText;
+        while(!decryptedText.equals(finalText)) {
+            key++;
+            decryptedText = SDESEncryptor.decrypt(encryptedText, key);
+            System.out.println("Key: " + key + "  DecryptedText: " + decryptedText);
+        }
     }
 }
