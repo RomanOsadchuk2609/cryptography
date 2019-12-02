0444b0ccc04ae9ab3af21b6d119fe33ead777378
Andrew Yesipcuk <yesipchuk.andriy@gmail.com>
2019-12-02 22:09:53 +0200

    Lab15

--- src/main/lab/Lab15.java
+++ src/main/lab/Lab15.java
@@ -4,26 +4,47 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.InvalidKeyException;
+import java.util.Scanner;
 import javax.xml.bind.DatatypeConverter;
 
 public class Lab15 {
 
     public static void main(String[] args) {
         try {
-            String key = "key";
-            String message = "message";
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter a message");
+            String message = keyboard.next();
+            System.out.println("Enter a key");
+            String key = keyboard.next();
 
             Mac hasher = Mac.getInstance("HmacSHA256");
             hasher.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));
 
             byte[] hash = hasher.doFinal(message.getBytes());
+            System.out.println("HMAC: ");
+            String HMAC1 = DatatypeConverter.printBase64Binary(hash);
+            System.out.println(HMAC1);
 
-            // to lowercase hexits
-            System.out.println(DatatypeConverter.printHexBinary(hash));
+            System.out.println("Checking HMAC: ");
+            System.out.println("Enter a message");
+            message = keyboard.next();
+            System.out.println("Enter a key");
+            key = keyboard.next();
 
-            // to base64
-            System.out.println(DatatypeConverter.printBase64Binary(hash));
+            hasher = Mac.getInstance("HmacSHA256");
+            hasher.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));
+
+            hash = hasher.doFinal(message.getBytes());
+            String HMAC2 = DatatypeConverter.printBase64Binary(hash);
+
+            if (HMAC1.equals(HMAC2)) {
+                System.out.println("HMAC is OK!");
+            } else {
+                System.out.println("HMAC is not OK!");
+            }
         }
-        catch (NoSuchAlgorithmException | InvalidKeyException ignored) {}
+        catch (NoSuchAlgorithmException | InvalidKeyException ignored) {
+            System.out.println("Пєчалька.");
+        }
     }
 }
