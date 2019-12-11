30286fa63b9ff72a3c7e1470261b2a5906fef9dd
Roman Osadchuk <osadchuk.roma.26@gmail.com>
2019-12-11 22:31:02 +0200

    lab14: mouse

--- src/main/lab/lab14/LinearCongruentialGenerator.java
+++ src/main/lab/lab14/LinearCongruentialGenerator.java
@@ -1,19 +1,14 @@
 package main.lab.lab14;
 
 public class LinearCongruentialGenerator {
-
-    private int max;
     private int last;
 
-    // constructor that takes the max int
-    public LinearCongruentialGenerator(int max) {
-        this.max = max;
-        last = (int) (System.currentTimeMillis() % max);
+    public LinearCongruentialGenerator(int last) {
+        this.last = last;
     }
 
-    // Note that the result can not be bigger then 32749
     public int nextInt() {
-        last = (last * 32719 + 3) % 32749;
-        return last % max;
+        last = (last * 32719 + 16954) % 32749;
+        return last;
     }
 }
--- src/main/lab/lab14/Main.java
+++ src/main/lab/lab14/Main.java
@@ -1,17 +1,25 @@
 package main.lab.lab14;
 
+import javax.sound.midi.Soundbank;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
+    private static final int LCG_MAX = 32749;
 
     public static void main(String... ignored) throws FileNotFoundException, UnsupportedEncodingException {
         //Size in Gbs of my file that I want
-        double wantedSize = Double.parseDouble(System.getProperty("size", "0.1"));
-
-        LinearCongruentialGenerator lcg = new LinearCongruentialGenerator(32749);
+        double wantedSize = Double.parseDouble(System.getProperty("size", "0.0125"));//12,5 mb
         String separator = " ";
-
+        List<Integer> mouseInputList = getMouseInput();
+        int hash = getHash(mouseInputList);
+        System.out.println("Hash: " + hash);
+        LinearCongruentialGenerator lcg = new LinearCongruentialGenerator(hash);
         File file = new File("AvgNumbers.txt");
         long start = System.currentTimeMillis();
         PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8")), false);
@@ -38,4 +46,29 @@ public class Main {
         System.out.printf("Took %.1f seconds to create a file of %.3f GB", time / 1e3, file.length() / 1e9);
     }
 
+    private static List<Integer> getMouseInput() {
+        System.out.println("Move the mouse");
+        List<Integer> mouseInputList = new ArrayList<>();
+        long lastMousePositionTime = 0;
+        while (mouseInputList.size() < 50) {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastMousePositionTime >= 200) {
+                PointerInfo a = MouseInfo.getPointerInfo();
+                Point b = a.getLocation();
+                int x = (int) b.getX();
+                int y = (int) b.getY();
+                System.out.println(x + " : " + y + " : " + x * y);
+                mouseInputList.add(x * y);
+                lastMousePositionTime = currentTime;
+            }
+        }
+        System.out.println("Stop moving mouse");
+        return mouseInputList;
+    }
+
+    private static Integer getHash(List<Integer> list) {
+        return list.stream()
+                .map(x -> x % LCG_MAX)
+                .reduce(0, Integer::sum) % LCG_MAX;
+    }
 }
